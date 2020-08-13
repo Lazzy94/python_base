8d14a1d8030ce5f8c1af5a33ecb318441f7153ca
Самусенков Андрей <samusenkov-andre@mail.ru>
2020-08-13 12:04:33 +0300

    zoo complete

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -7,17 +7,21 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
-# TODO здесь ваш код
-
+zoo.insert(1,'bear')
+print(zoo)
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark']
 #  и выведите список на консоль
-# TODO здесь ваш код
+#zoo =zoo+birds
+zoo.extend(birds)
+print(zoo)
 
 # уберите слона
 #  и выведите список на консоль
-# TODO здесь ваш код
-
+del zoo[3]
+print(zoo)
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+zoo.index('lion')
+print(zoo.index('lion')+1)
+print(zoo.index('lark')+1)
